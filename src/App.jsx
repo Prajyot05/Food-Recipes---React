d1b4e5bb2d0fae0287cc7132bfab05897847698d
Prajyot Tayde <prajyottayde@Prajyots-MacBook-Air.local>
2024-05-10 11:05:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/index'
+import Home from './pages/home'
+import Favorites from './pages/favorites'
+import Details from './pages/details'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='/recipe-item/:id' element={<Details />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
